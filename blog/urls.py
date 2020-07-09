2407ef0748f7554f67fa69b1a91d2b1fd35d96b5
Grant Shanklin <grantshanklin@gmail.com>
2020-07-09 15:53:09 -0500

    Multple view now working

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #url('', views.home, name='blog-home'),
+    url('about', views.about, name='blog-about'),
+    url('', views.home, name='blog-home'),
+]
