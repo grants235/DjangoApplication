3779e68578c9985abcfdb5987f7b132539aac3de
Grant Shanklin <grantshanklin@gmail.com>
2020-07-09 16:47:35 -0500

    Added templates and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Grant',
+        'title': 'Blog Post 1',
+        'content': 'This is the content of blog post 1',
+        'date_posted': 'July 8, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is the content of blog post 2',
+        'date_posted': 'July 9, 2020'
+    },
+    {
+        'author': 'John Smith',
+        'title': 'Blog Post 3',
+        'content': 'This is the content of blog post 3',
+        'date_posted': 'July 10, 2020'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
