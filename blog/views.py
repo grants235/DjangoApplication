2390217752690baf6f1789ddf2c51a02c13cba00
Grant Shanklin <grantshanklin@gmail.com>
2020-07-09 22:09:52 -0500

    Added database

--- blog/views.py
+++ blog/views.py
@@ -2,33 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Grant',
-        'title': 'Blog Post 1',
-        'content': 'This is the content of blog post 1',
-        'date_posted': 'July 8, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'This is the content of blog post 2',
-        'date_posted': 'July 9, 2020'
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Blog Post 3',
-        'content': 'This is the content of blog post 3',
-        'date_posted': 'July 10, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
